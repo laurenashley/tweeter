3c53725a6f8d807d61157eb2515110b98e27f37c
Lauren Ashley <la.zaubi@gmail.com>
2022-11-05 16:15:33 -0700

    hardcode tweet obj for now, implement create tweet fn

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,22 @@
+/* eslint-disable quote-props */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweet = {
+  'user': {
+    'name': 'Newton',
+    'avatars': 'https://i.imgur.com/73hZDYK.png',
+    'handle': '@SirIsaac',
+  },
+  'content': {
+    'text': 'If I have seen further it is by standing on the shoulders of giants',
+  },
+  'created_at': 1461116232227,
+};
+
+const createTweetElement = () => {
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+};
