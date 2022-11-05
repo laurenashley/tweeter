33175aad0570cfc87e917a0687c7b5d3e9ce8551
Lauren Ashley <la.zaubi@gmail.com>
2022-11-04 21:48:49 -0700

    implement counter update new count as user types in textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,13 @@
 /* eslint-disable prefer-arrow-callback */
 $(document).ready(function() {
-  console.log('char counter script is running!');
+  const initCount = $('output.counter').val();
 
   // eslint-disable-next-line space-before-blocks
   $('#tweet-text').keyup(function() {
-    console.log($(this).val().length);
     const inputLength = $(this).val().length;
     const counter = $('output.counter');
-    console.log(counter.val() - inputLength);
-    // let newCounterVal = counter.val() - inputLength;
-    counter.val(counter.val() - inputLength);
-    // To Do if counter val < 0 turn number red
-    if (counter.val() < 0) {
-      counter.addClass('red-text');
-    } else {
-      counter.removeClass('red-text');
-    }
+    counter.val(initCount - inputLength);
+    // make text red when input is over the limit
+    counter[counter.val() < 0 ? 'addClass' : 'removeClass']('red-text');
   });
 });
