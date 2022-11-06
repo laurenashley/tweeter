6d2e05654a1d925aba06db5546985565decd5056
Lauren Ashley <la.zaubi@gmail.com>
2022-11-05 17:50:51 -0700

    make sure page loads before script runs, append tweet tp feed container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,48 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// eslint-disable-next-line prefer-arrow-callback
+$(document).ready(function() {
+  const createTweetElement = (tweetData) => {
+    const { user } = tweetData;
+    const { content } = tweetData;
+    const { created_at } = tweetData;
+    const $tweet = $(`
+      <article>
+        <header>
+          <span><img src="${user.avatar}" />${user.name}</span>
+          <span class="username">${user.handle}</span>
+        </header>
+        <p>${content.text}</p>
+        <footer>
+          <span>${created_at} ago</span>
+          <div>
+            <i class="fa-solid fa-flag fa-xs"></i>
+            <i class="fa-solid fa-retweet fa-xs"></i>
+            <i class="fa-solid fa-heart fa-xs"></i>
+          </div>
+        </footer>
+      </article>
+    `);
 
-const createTweetElement = (tweetData) => {
-  const { user } = tweetData;
-  const { content } = tweetData;
-  const { created_at } = tweetData;
-  const $tweet = $(`
-    <article>
-      <header>
-        <span><img src="${user.avatar}" />${user.name}</span>
-        <span class="username">${user.handle}</span>
-      </header>
-      <p>${content}</p>
-      <footer>
-        <span>${created_at} ago</span>
-        <div>
-          <i class="fa-solid fa-flag fa-xs"></i>
-          <i class="fa-solid fa-retweet fa-xs"></i>
-          <i class="fa-solid fa-heart fa-xs"></i>
-        </div>
-      </footer>
-    </article>
-  `);
+    return $tweet;
+  };
 
-  return $tweet;
-};
+  const tweetData = {
+    'user': {
+      'name': 'Newton',
+      'avatar': 'https://i.imgur.com/73hZDYK.png',
+      'handle': '@SirIsaac',
+    },
+    'content': {
+      'text': 'If I have seen further it is by standing on the shoulders of giants',
+    },
+    'created_at': 1461116232227,
+  };
 
-const tweetData = {
-  'user': {
-    'name': 'Newton',
-    'avatars': 'https://i.imgur.com/73hZDYK.png',
-    'handle': '@SirIsaac',
-  },
-  'content': {
-    'text': 'If I have seen further it is by standing on the shoulders of giants',
-  },
-  'created_at': 1461116232227,
-};
+  const $tweet = (createTweetElement(tweetData));
 
-const $tweet = (createTweetElement(tweetData));
+  console.log($tweet);
 
-console.log($tweet);
-
-$('section#tweet-feed').append($tweet.innerHTML);
+  $('#tweet-feed').prepend($tweet);
+});
