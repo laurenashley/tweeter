d3281c4fbc94ebf76a197ae9cb4c81c837804b6f
Lauren Ashley <la.zaubi@gmail.com>
2022-11-22 10:48:14 -0800

    add scroll up module for stretch feature

---
+++ public/scripts/scroll-up.js
@@ -0,0 +1,7 @@
+/* eslint-disable prefer-arrow-callback */
+$(document).ready(function() {
+  // Show scroll up button n% down page
+
+  // scrol to tween form and focu textarea when scroll button clicked
+  
+});
